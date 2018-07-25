5b9e2a857592279a8834e17ef490526a11911df6
Nick Zhang <thesadboy@qq.com>
2018-07-25 15:27:09 +0800

    删除axios

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Axios from 'axios';
 
 Vue.config.productionTip = false;
-Vue.prototype.$http = Axios;
 
 new Vue({
     render: h => h(App)
