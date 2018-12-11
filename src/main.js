4312f1e773a342548b2a40582bfaca2162b352f5
Nick Zhang <thesadboy@qq.com>
2018-12-11 10:19:09 +0800

    更新whistle未启动时的样式

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import {Input, Button, Form, FormItem} from 'element-ui';
+import {Input, Button, Form, FormItem, Card} from 'element-ui';
 
 Vue.config.productionTip = false;
 Vue.component(Input.name, Input);
 Vue.component(Button.name, Button);
 Vue.component(Form.name, Form);
 Vue.component(FormItem.name, FormItem);
+Vue.component(Card.name, Card);
 
 new Vue({
     render: h => h(App)
